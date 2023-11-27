26fb132fcb38f4a934ee216d7eff2ed37abb0390
Henri Staudenrausch <henri@staudenrausch.de>
2023-11-27 22:30:58 +0100

    implemented DLXPentominoTWYX

--- src/java/uebung6/DLXPentominoTWYX.java
+++ src/java/uebung6/DLXPentominoTWYX.java
@@ -1,5 +1,78 @@
 package uebung6;
 
-public class DLXPentominoTWYX {
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 
+public class DLXPentominoTWYX {
+    private static Pentomino[] pentominos = new Pentomino[20];
+    private static List<BitSet> initialProblem = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int n;
+        try {
+            n = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Usage: java DLXPentominoTWYX <n>");
+            return;
+        }
+
+        pentominos[0] = new Pentomino(new int[]{0, 1, 2, 6, 11}); // T's
+        pentominos[1] = new Pentomino(new int[]{0, 5, 6, 7, 10});
+        pentominos[2] = new Pentomino(new int[]{1, 6, 10, 11, 12});
+        pentominos[3] = new Pentomino(new int[]{2, 5, 6, 7, 12});
+        pentominos[4] = new Pentomino(new int[]{0, 1, 6, 7, 12}); // W's
+        pentominos[5] = new Pentomino(new int[]{0, 1, 6, 11, 12});
+        pentominos[6] = new Pentomino(new int[]{1, 2, 5, 6, 10});
+        pentominos[7] = new Pentomino(new int[]{2, 6, 7, 10, 11});
+        pentominos[8] = new Pentomino(new int[]{1, 5, 6, 7, 11}); // X
+        pentominos[9] = new Pentomino(new int[]{1, 5, 6, 11, 16}); // Y's
+        pentominos[10] = new Pentomino(new int[]{1, 6, 10, 11, 16});
+        pentominos[11] = new Pentomino(new int[]{0, 5, 6, 10, 15});
+        pentominos[12] = new Pentomino(new int[]{0, 5, 10, 11, 15});
+        pentominos[13] = new Pentomino(new int[]{0, 1, 2, 3, 6});
+        pentominos[14] = new Pentomino(new int[]{0, 1, 2, 3, 7});
+        pentominos[15] = new Pentomino(new int[]{1, 5, 6, 7, 8});
+        pentominos[16] = new Pentomino(new int[]{2, 5, 6, 7, 8});
+
+        for (Pentomino pentomino : pentominos) {
+            for (int x = 0; x < 5 - pentomino.getWidth(); x++) {
+                for (int y = 0; y < n - pentomino.getHeight(); y++) {
+                    initialProblem.add(pentomino.getBitSet(x, y));
+                }
+            }
+        }
+
+        DLXSolver solver = new DLXSolver(initialProblem.toArray(new BitSet[initialProblem.size()]), 5 * n);
+    }
+
+    private static class Pentomino {
+        private int[] shape;
+        private int width;
+        private int height;
+
+        public Pentomino(int[] shape) {
+            this.shape = shape;
+            for (int i : shape) {
+                width = Math.max(width, i % 5);
+                height = Math.max(height, i / 5);
+            }
+        }
+
+        public BitSet getBitSet(int xShift, int yShift) {
+            BitSet bitSet = new BitSet(shape[shape.length - 1] + xShift + yShift * 5 + 1);
+            for (int i : shape) {
+                bitSet.set(i + xShift + yShift * 5);
+            }
+            return bitSet;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+    }
 }
